6ee36bdd2f037edab5a5d5c592be5cd9b1e440d6
Tim Miller <timmil@cisco.com>
2021-03-05 13:45:16 -0500

    Add one resource creation and data source reference to make the example interesting from an 'apply' perspective.

--- 01-simple-inventory/main.tf
+++ 01-simple-inventory/main.tf
@@ -13,3 +13,15 @@ provider "intersight" {
     secretkey = var.intersight_secretkey
     endpoint = var.intersight_endpoint
 }
+
+## Get the MO data for the default organization in the account.
+# /api/v1/organization/Organizations$filter=(Name eq 'default')
+data "intersight_organization_organization" "default" {
+    name = "default"
+}
+
+## Create a new organization resource for this demo
+resource "intersight_organization_organization" "target" {
+    description = "Terraform Deployed"
+    name = var.target_organization
+}
